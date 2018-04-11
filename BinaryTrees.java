0b8efb16074d4c3506ff2bac9f2d122b29984972
Disbekiki <enriqueperez@cpp.edu>
2018-04-10 21:15:06 -0700

    Parts for CS241 Hw 2

---
+++ BinaryTrees.java
@@ -0,0 +1,9 @@
+package PracticeWithBinaryTrees;
+import StackPackage.*;
+import TreePackage.*;
+public class BinaryTrees {
+
+	public static void main(String[] args) {
+		GuessingGame game = new GuessingGame("Does your animal eat Meat?", "Cow","Dog");
+	}
+}
