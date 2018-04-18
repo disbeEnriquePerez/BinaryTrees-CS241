d6605c7e7c9e1534f3a77d7442e3cc9be2febbe3
Enrique Perez <enriqueperez@cpp.edu>
2018-04-17 20:07:10 -0700

    Delete BinaryTree.java

--- BinaryTree.java
+++
@@ -1,9 +0,0 @@
-package PracticeWithBinaryTrees;
-import StackPackage.*;
-import TreePackage.*;
-public class BinaryTrees {
-
-	public static void main(String[] args) {
-		GuessingGame game = new GuessingGame("Does your animal eat Meat?", "Cow","Dog");
-	}
-}
